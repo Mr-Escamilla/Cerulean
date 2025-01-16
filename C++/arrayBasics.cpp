fad9e091e65eb554ae7bc9eabd96c3ef49850632
Mr-Escamilla <aescamilla@long-view.com>
2025-01-16 08:35:20 -0600

    learning data types & arrays

--- C++/arrayBasics.cpp
+++ C++/arrayBasics.cpp
@@ -6,25 +6,28 @@ using namespace std;
 
 // Main() function: where the execution of
 // program begins
-int main()
+int main(void)
 {
     // Prints hello world
     cout << "Hello World" << endl;
     
     cout << "Size of char: " << sizeof(char) << endl;
     cout << "Size of int: " << sizeof(int) << endl;
-
-    cout << "Size of long: " << sizeof(long) << endl;
     cout << "Size of float: " << sizeof(float) << endl;
 
+    cout << "Size of long: " << sizeof(long) << endl;
     cout << "Size of double: " << sizeof(double) << endl;
 
     char aiden[] = "escamilla";
     char* pointer = aiden;
-    cout << *(aiden + 4) << endl;
+    cout << *(aiden + 9) << endl;
 
-    int five[] = {0, 1, 2, 3, 4};
+    int five[] = {0, 1, 3, 5, 7};
     int* point = five;
-    cout << point << endl;
+    
+    cout << &five << endl;
+    cout << *&(five[2]) << endl;
+    cout << point[2] << endl;
+
     return 0;
 }
---
+++ C++/cPlusPlus101.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+int main(void){
+  cout << "Hello World" << endl;
+  
+  // string names[] = {"Mela", "Julia", "Amelia", "Henry", "Aaron"};
+
+  // int test = 4;
+  
+  // char test = (char) 69;
+  // cout << test << endl;
+
+
+  // for(int i = 0; i < 5; i++){
+  //   if (names[i] == "Mahin"){
+  //     cout << "Found" << endl;
+  //     return 0;
+  //   }
+  // }
+  // cout << "Not Found" << endl;
+  // return 1;
+}
